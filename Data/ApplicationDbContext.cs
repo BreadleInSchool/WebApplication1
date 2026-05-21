11803c8e5a08559ee6e67d43896177b59f5a7b41
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2026-05-21 12:43:18 +0800

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Contracts;
+
+namespace WebApplication1.Data
+
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public class ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+
+       public Db
+
+    }
+}
